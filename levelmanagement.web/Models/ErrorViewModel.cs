b42feeded23721a10a0296ed0ca2b418b3f9e816
vnwef <vnwef@LAPTOP-EFOG3EI9>
2021-12-01 09:23:32 +0000

    Add project files.

---
+++ levelmanagement.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace levelmanagement.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
